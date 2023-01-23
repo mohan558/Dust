49a02654614815e55958643d2bd44ec2b0813970
mohan558 <gkannan741@gmail.com>
2023-01-23 15:01:05 +0530

    Hi

---
+++ src/test/java/org/git/test/GitTaskClass.java
@@ -0,0 +1,16 @@
+package org.git.test;
+
+public class GitTaskClass {
+	private void m1() {
+		System.out.println("1");
+
+	}
+	private void m2() {
+		System.out.println("2");
+
+	}
+	private void m3() {
+		System.out.println("3");
+	}
+
+}
