f88b06b93fe602df4807bc81299d66380ec70fd3
mohan558 <gkannan741@gmail.com>
2023-01-23 15:38:26 +0530

    Method Added

--- src/test/java/org/git/test/GitTaskClass.java
+++ src/test/java/org/git/test/GitTaskClass.java
@@ -12,5 +12,14 @@ public class GitTaskClass {
 	private void m3() {
 		System.out.println("3");
 	}
+	private void m7() {
+		System.out.println("7");
+	}
+	private void m8() {
+		System.out.println("8");
+	}
+	private void m9() {
+		System.out.println("9");
+	}
 
 }
